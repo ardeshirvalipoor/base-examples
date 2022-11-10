0e3f7f44ecc105eaa3ebcb59c4907cc90dbea534
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-11-10 16:40:47 +0100

    readme updated

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import * as cors from 'cors'
 import routes from './routes/index'
+import * as jwt from 'jsonwebtoken'
 
 const PORT = process.env.PORT || 7001
 const app = express()
@@ -13,5 +14,3 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ limit: 1024102420, type: 'application/json' }))
 app.use(routes)
 app.listen(PORT, () => console.log('Todo api listening on port', PORT))
-
-// check the output: jwt.verify(token, secret)
